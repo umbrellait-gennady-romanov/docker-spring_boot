1607f7ad1ac1c8cc06b34c8343e15200fb1b4750
gennadyromanov <gennady.romanov@umbrellait.com>
2020-12-08 12:40:59 +0300

    method-controller

--- src/main/java/com/umbrellait/docker_spring_boot/controller/HTTPController.java
+++ src/main/java/com/umbrellait/docker_spring_boot/controller/HTTPController.java
@@ -8,17 +8,27 @@ public class HTTPController {
 
     @GetMapping
     public String responseGet() {
-        return "{“method”:“GET”, “text”: “Hello world”}";
+
+        return "{“method”:“GET”, “text”: “Queries using Get can only retrieve data”}";
     }
 
     @PutMapping
     public String responsePut() {
-        return "{“method”:“PUT”, “text”: “Hello world”}";
+        return "{“method”:“PUT”, “text”: “Used to replace an existing object.”}";
     }
 
     @PostMapping
     public String responsePost() {
-        return "{“method”:“POST”, “text”: “Hello world”}";
+        return "{“method”:“POST”, “text”: “Sends data to the server to create a new object.”}";
     }
 
+    @DeleteMapping
+    public String responseDelete() {
+        return "{“method”:“Delete”, “text”: “Delete removes the specified resource.”}";
+    }
+
+    @PatchMapping
+    public String responsePatch() {
+        return "{“method”:“Patch”, “text”: “Used to partially modify a resource.”}";
+    }
 }
