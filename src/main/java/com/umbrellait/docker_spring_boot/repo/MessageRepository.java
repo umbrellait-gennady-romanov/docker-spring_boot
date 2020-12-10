a57e1acd350e62961e0a3ede441db5627eb49ba7
gennadyromanov <gennady.romanov@umbrellait.com>
2020-12-10 17:35:52 +0300

    properties_bag_flyway

--- src/main/java/com/umbrellait/docker_spring_boot/repo/MessageRepository.java
+++ src/main/java/com/umbrellait/docker_spring_boot/repo/MessageRepository.java
@@ -3,14 +3,16 @@ package com.umbrellait.docker_spring_boot.repo;
 import com.umbrellait.docker_spring_boot.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     @Query(
-            value = "Select * from message m where m.date Between :startDate and :endDate AND method LIKE ':search%'",
+            value = "Select * from message where date Between :startDate and :endDate AND method LIKE ':search%'",
             nativeQuery = true)
     List<Message> getMessageList(LocalDateTime startDate, LocalDateTime endDate, String search);
 }
